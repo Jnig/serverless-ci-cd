6b220a9d5c5dbcfd725d7394d65236b106e06d66
Jakob <info@jakobniggel.de>
2019-05-25 12:02:55 +0200

    Update mirror.sh

--- docker/mirror.sh
+++ docker/mirror.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
-echo "$SSH_KEY" > /tmp/key && chmod 400 /tmp/key 
+git config --global credential.helper '!aws codecommit credential-helper $@' 
+git config --global credential.UseHttpPath true
 
-GIT_SSH_COMMAND="ssh -i /tmp/key" git clone --mirror $source
-git push --mirror $target
+aws secretsmanager get-secret-value --secret-id $secret_name | jq -r '.SecretString' > /tmp/key && chmod 400 /tmp/key
+
+GIT_SSH_COMMAND="ssh -i /tmp/key -o StrictHostKeyChecking=no " git clone --mirror $source source
+cd source && git push --mirror $target
